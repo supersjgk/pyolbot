25a0e9e559d7b45b4c2f85a1fb30a567eb43c603
ssj7 <tomtomforgeek@gmail.com>
2024-09-03 20:05:56 -0500

    path_finder

--- pyolbot/path_finder.py
+++ pyolbot/path_finder.py
@@ -1,5 +1,7 @@
 import winreg
-
+import os
+import json
+    
 def find_chrome_path():
     try:
         key_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe"
@@ -8,14 +10,49 @@ def find_chrome_path():
             return chrome_path
     except FileNotFoundError:
         return None
+    finally:
+        curdir = os.getcwd()
+        config_path = os.path.join(curdir, f"config.json")
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+        config["chrome_path"] = chrome_path
+        with open(config_path, 'w') as f:
+            json.dump(config, f, indent=4)
     
 def find_python_path():
-    pass
-    """
     try:
         key_path = r"SOFTWARE\Python\PythonCore"
         with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path) as key:
-            version_path = winreg.EnumKey(key, 0)
+            i = 0
+            versions = []
+            while True:
+                try:
+                    version = winreg.EnumKey(key, i)
+                    versions.append(version)
+                    i += 1
+                except OSError:
+                    break
+        for version in versions:
+            try:
+                version_path = f"{key_path}\\{version}\\InstallPath"
+                with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, version_path) as version_key:
+                    install_path, _ = winreg.QueryValueEx(version_key, "")
+                    python_path = os.path.join(install_path, "python.exe")
+                    if os.path.isfile(python_path):
+                        return python_path
 
-    except:
-    """
+            except Exception as e:
+                continue
+        return None
+    except Exception as e:
+        print(f'{e}')
+    finally:
+        curdir = os.getcwd()
+        config_path = os.path.join(curdir, f"config.json")
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+        config["python_path"] = python_path
+        with open(config_path, 'w') as f:
+            json.dump(config, f, indent=4)
+
+# print(find_python_path())
